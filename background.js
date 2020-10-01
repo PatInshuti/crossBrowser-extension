1a5f0315b9c1d43e140de99c5d2afd0a0e4d61d3
PatInshuti <pim219@nyu.edu>
2020-10-01 04:33:29 +0400

    basic

---
+++ background.js
@@ -0,0 +1,40 @@
+// Load the javascript file of features
+
+chrome.runtime.getPackageDirectoryEntry(function(root) {
+    root.getFile("FinalFeatures.txt", {}, function(fileEntry) {
+      fileEntry.file(function(file) {
+        var reader = new FileReader();
+
+        reader.onloadend = function(e) {
+        //   console.log(reader.result)
+        };
+        reader.readAsText(file);
+      });
+    });
+});
+
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+        if (details.type == "script"){
+
+            fetch(details.url).then(r => r.text()).then(result => {
+                // Result now contains the response text, do what you want...
+                console.log(result)
+            })
+
+            // console.log(details)
+
+            // var oReq = new XMLHttpRequest();
+
+
+            // var res = oReq.open("GET",details.url);
+
+            // console.log(res)
+            
+            // oReq.send();
+
+        }
+    },
+{urls: ["<all_urls>"]},
+["blocking"]);
