746a25d89721a9e0fceb5d340533e536c5a996b8
PatInshuti <pim219@nyu.edu>
2020-12-09 17:59:05 +0400

    big time progress

--- background.js
+++ background.js
@@ -51,84 +51,185 @@ const setupDB = async (data) =>{
         }
 
         else{
+            // Fetch Features Locally
+            await getFeatures('selectedFeatures.txt').then(_res => {
 
-                // Fetch Features Locally
-                await getFeatures('selectedFeatures.txt').then(_res => {
-
-                    lines = _res.split('\n');
-                    
-                    lines.forEach(async line=>{
-                        await featuresList.push(line)
-                    })
+                lines = _res.split('\n');
+                
+                lines.forEach(async line=>{
+                    await featuresList.push(line)
                 })
+            })
+        
+            //open the db 
+            const request = window.indexedDB.open(db_name,2);
+
+            request.onerror = (event) =>{
+                console.log("error opening db...")
+            }
             
-                //open the db 
-                const request = window.indexedDB.open(db_name,2);
+            // Runs only one time
+            request.onupgradeneeded = async (event) =>{
 
-                request.onerror = (event) =>{
-                    console.log("error opening db...")
-                }
+                db = event.target.result;
                 
-                // Runs only one time
-                request.onupgradeneeded = async (event) =>{
-
-                    db = event.target.result;
-                    
-                    let featureObjectStore = db.createObjectStore(featureStore,{autoIncrement:true});
-                    featureObjectStore.transaction.oncomplete = () =>{
-                        console.log("feature store created")
-                    }
-
-                    let hashCodeToScriptObjectStore = db.createObjectStore(hashCodeToScriptStore, {keyPath: 'id'});
-                    hashCodeToScriptObjectStore.transaction.oncomplete = () =>{
-                        console.log("hashcode_to_script store created")
-                    }
-
-                    await featuresList.forEach(async feature=>{
-                        await featureObjectStore.add(feature)
-                    })
-
+                let featureObjectStore = db.createObjectStore(featureStore,{autoIncrement:true});
+                featureObjectStore.transaction.oncomplete = () =>{
+                    console.log("feature store created")
                 }
-                
-                // // Runs 
-                // request.onsuccess = (event) =>{
-                //     db = event.target.result;
-    
-                //     if (!db.objectStoreNames.contains(featureStore)){
-                //         console.log("store does not exist");
-                //     }
-                //     else{
-                //         console.log("store exists")
-    
-                //         var tx = db.transaction(featureStore, 'readwrite');
-                //         var store = tx.objectStore(featureStore)
-    
-                //         // Go through line by line and store in db
-                        
-                //         // var request = store.add("okay this is my input");
-    
-                //         // request.onerror = function(e) {
-                //         //     console.log('Error', e.target.error.name);
-                //         // };
-    
-                //         // request.onsuccess = function(e) {
-                //         // console.log('Woot! Did it');
-                //         // };
-    
-                //         var getall = store.getAll();
-    
-                //         getall.onsuccess = (event) =>{
-                //             console.log(event.target.result);
-                //         }
-                //     }
-    
-                //     // db.deleteObjectStore("tests");
-                // }
- 
+
+                let hashCodeToScriptObjectStore = db.createObjectStore(hashCodeToScriptStore, {keyPath: 'id'});
+                hashCodeToScriptObjectStore.transaction.oncomplete = () =>{
+                    console.log("hashcode_to_script store created")
+                }
+
+                await featuresList.forEach(async feature=>{
+                    await featureObjectStore.add(feature)
+                })
+
+            }
         }
 }
 
+const runScriptLabelling = (featureIndexMapping, model) =>{
+    browser.webRequest.onBeforeSendHeaders.addListener( async (details) => {
+        if (details.type == "script"){
+
+
+
+            await fetch(details.url).then(r => r.text()).then(async result => {
+                // First check if script hashcode exists in the objectStore
+
+
+                // Get the hash string
+                let hashValue = await hashString(result);
+
+                var tx2 = db.transaction(hashCodeToScriptStore, 'readwrite');
+                var hashCodeToScriptDBStore = tx2.objectStore(hashCodeToScriptStore);
+
+                var getAllhashCodeToScript = hashCodeToScriptDBStore.get(hashValue);
+
+                getAllhashCodeToScript.onsuccess = async (event) =>{
+
+                    // Start intercepting requests
+                    theMapping = event.target.result;
+
+                    // If we do not find the hascode for the script
+                    if (theMapping == undefined){
+
+                        let featuresCount = {}
+
+                        // Replace all multiple consecutive white spaces with one white space
+                        result = result.replace(/\s+/g, ' ')
+
+                        featuresList.forEach(feature =>{
+
+                            if (!feature.includes("__")){
+                                let searchTerm = "."+feature+"\\("
+                                let searchTerm2 = "."+feature+" \\("
+                                let count = result.search(searchTerm);
+                                let count2 =  result.search(searchTerm2) 
+                                count == -1 ? featuresCount[feature] = 0 : featuresCount[feature] = count
+                                count2 == -1 ? "" : featuresCount[feature] += count2
+                            }
+
+                            else{
+                                let feats = feature.split('__')
+                                let res = 1
+
+                                feats.forEach(feat=>{
+                                    let searchTerm = "."+feat+"\\("
+                                    let searchTerm2 = "."+feature+" \\("
+                                    let count = result.search(searchTerm)
+                                    let count2 =  result.search(searchTerm2)
+
+                                    if (count == -1 || count2 == -1)
+                                        res = 0
+
+                                })
+
+                                featuresCount[feature] = res
+                            }
+
+                        })
+
+                        let scriptArrayData = [[]];
+                        for (let i=0; i<508; ++i) scriptArrayData[0][i] = 0;
+
+                        // Filling the scriptArrayData with feature occurences
+                        await featuresList.forEach(async feature =>{
+                            if (featuresCount[feature] !=0){
+                                //get features's index on the list
+                                featureIndex = featureIndexMapping[feature]
+                                scriptArrayData[0][featureIndex] = featuresCount[feature];
+                            }
+                        })
+
+                        let newDataTensor = tf.tensor2d(
+                            scriptArrayData,
+                            [1, 508]
+                        );                
+
+                        predictions = model.predict(newDataTensor)  
+
+                        let maxProbability = Math.max(...predictions.dataSync());
+                        let predictionIndex = predictions.dataSync().indexOf(maxProbability);
+
+                        // Initialize db
+                        var tx2 = db.transaction(hashCodeToScriptStore, 'readwrite');
+                        var hashCodeToScriptDBStore = tx2.objectStore(hashCodeToScriptStore);
+
+                        let hashScriptMapping = {
+                            id: hashValue,
+                            label: classes[predictionIndex]
+                        };
+
+                        var request = hashCodeToScriptDBStore.add(hashScriptMapping);
+
+                        request.onerror = function(e) {
+                            console.log('Error', "Script hash code already exists");
+                        };
+
+                        request.onsuccess = function(e) {
+                        console.log('Added a new hascode script');
+                        };
+                        
+                    
+                        console.log("                               ")
+                        console.log("******************************")
+                        console.log(hashValue)
+                        console.log(details.url)
+                        console.log("predicted class -- "+ classes[predictionIndex])
+                        console.log("******************************")
+                        console.log("                               ")
+                    }
+
+                    else{
+
+                        console.log("******************************")
+                        console.log("                               ")
+
+                        console.log("script hashcode to label mapping already exists -- retrieving label")
+                        console.log(details.url)
+                        console.log(theMapping)
+
+                        console.log("******************************")
+                        console.log("                               ")
+
+                    }
+                }
+
+            })
+
+        }
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking"]);
+}
+
 setupDB();
+
+
 
 classes = ["ads+marketing", "tag-manager+content", "hosting+cdn", "video", "utility", "analytics", "social", "customer-success"]
 
@@ -156,123 +257,14 @@ tf.loadLayersModel(browser.extension.getURL("model/model.json")).then( model=> {
                 console.log("All stores exist")
 
                 var tx = db.transaction(featureStore, 'readwrite');
-
                 var featureDBStore = tx.objectStore(featureStore);
-
-
-
-
                 var getallFeatures = featureDBStore.getAll();
-                // var getAllhashCodeToScript = hashCodeToScriptDBStore.getAll()
 
                 getallFeatures.onsuccess = (event) =>{
 
                     // Start intercepting requests
                     featuresList = event.target.result;
-                    browser.webRequest.onBeforeSendHeaders.addListener( async (details) => {
-                        if (details.type == "script"){
-
-
-                            await fetch(details.url).then(r => r.text()).then(async result => {
-
-                                let featuresCount = {}
-
-                                // Replace all multiple consecutive white spaces with one white space
-                                result = result.replace(/\s+/g, ' ')
-
-                                featuresList.forEach(feature =>{
-
-                                    if (!feature.includes("__")){
-                                        let searchTerm = "."+feature+"\\("
-                                        let searchTerm2 = "."+feature+" \\("
-                                        let count = result.search(searchTerm);
-                                        let count2 =  result.search(searchTerm2) 
-                                        count == -1 ? featuresCount[feature] = 0 : featuresCount[feature] = count
-                                        count2 == -1 ? "" : featuresCount[feature] += count2
-                                    }
-
-                                    else{
-                                        let feats = feature.split('__')
-                                        let res = 1
-
-                                        feats.forEach(feat=>{
-                                            let searchTerm = "."+feat+"\\("
-                                            let searchTerm2 = "."+feature+" \\("
-                                            let count = result.search(searchTerm)
-                                            let count2 =  result.search(searchTerm2)
-
-                                            if (count == -1 || count2 == -1)
-                                                res = 0
-
-                                        })
-
-                                        featuresCount[feature] = res
-                                    }
-
-                                })
-
-                                let scriptArrayData = [[]];
-                                for (let i=0; i<508; ++i) scriptArrayData[0][i] = 0;
-
-                                // Filling the scriptArrayData with feature occurences
-                                await featuresList.forEach(async feature =>{
-                                    if (featuresCount[feature] !=0){
-                                        //get features's index on the list
-                                        featureIndex = featureIndexMapping[feature]
-                                        scriptArrayData[0][featureIndex] = featuresCount[feature];
-                                    }
-                                })
-
-                                let newDataTensor = tf.tensor2d(
-                                    scriptArrayData,
-                                    [1, 508]
-                                );                
-
-                                predictions = model.predict(newDataTensor)  
-
-                                let maxProbability = Math.max(...predictions.dataSync());
-                                let predictionIndex = predictions.dataSync().indexOf(maxProbability);
-
-                                // Get the hash string
-                                let hashValue = await hashString(result);
-
-                                const request = window.indexedDB.open(db_name,2);
-
-                                request.onsuccess = (event) =>{
-                                    db = event.target.result;
-
-                                    var tx2 = db.transaction(hashCodeToScriptStore, 'readwrite');
-
-                                    var hashCodeToScriptDBStore = tx2.objectStore(hashCodeToScriptStore);
-
-                                    // var request = hashCodeToScriptDBStore.add("okay this is my input");
-
-                                    // request.onerror = function(e) {
-                                    //     console.log('Error', e.target.error.name);
-                                    // };
-                
-                                    // request.onsuccess = function(e) {
-                                    // console.log('Woot! Did it');
-                                    // };
-                                    
-                                
-                                    console.log("                               ")
-                                    console.log("******************************")
-                                    console.log(hashValue)
-                                    console.log(details.url)
-                                    console.log("predicted class -- "+ classes[predictionIndex])
-                                    console.log("******************************")
-                                    console.log("                               ")
-                                    hashScriptMapping[hashValue] = classes[predictionIndex]
-                                    console.log(hashScriptMapping)
-                                }
-
-                            })
-
-                        }
-                    },
-                    {urls: ["<all_urls>"]},
-                    ["blocking"]);
+                    runScriptLabelling(featureIndexMapping,model);
 
                 }
             }
