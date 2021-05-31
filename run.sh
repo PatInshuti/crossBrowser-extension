dfe4188b38c31675513688e89eb1ad3a81a508a8
PatInshuti <pim219@nyu.edu>
2021-05-31 16:20:15 +0400

    .

--- run.sh
+++ run.sh
@@ -6,13 +6,19 @@ COUNTER=0
 #!/bin/bash
 while IFS= read -r url; do
 
-    browsertime --android --browser firefox $url
-    
+    # with extension
+    browsertime --android --prettyPrint --browsertime.cacheClearRaw --firefox.preference browser.cache.disk.enable:false --extension ../../../extension/web-ext-artifacts/capstone.xpi --browser firefox $url https://google.com $url --multi --output withExtension.json
+
+    # with no extension
+    browsertime --android --prettyPrint --browsertime.cacheClearRaw --firefox.preference browser.cache.disk.enable:false --browser firefox $url --output noExtension.json
+
+
     COUNTER=$((COUNTER+1))
-    
-    if (( COUNTER %20 == 0 ))
+   
+    if (( COUNTER %30 == 0 ))
     then
         sleep 300 #5 minutes
     fi
+    
     
 done < "$1"
